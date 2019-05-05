3052a5d0d0fb7551fbd052a70ef98ee805192b35
Debora Duarte <debora.duarteas@gmail.com>
2019-05-05 18:56:00 -0300

    Update exercise1.rb

--- confidence/exercise1.rb
+++ confidence/exercise1.rb
@@ -1,3 +1,8 @@
+# This code is written in Ruby. And this line is a comment in Ruby.
+# Create a ruby project on Repl.it.
+# Do not be afraid if you don't know ruby: just try fixing it. 
+# The suggestion is: run the code, take the error message and Google it to try to find out and fix the problem.
+
 umavariavelcomnomegrande = 1
 outravariavelcomnomegrande = 1 + umavariavelcomnomegrande
 puts outrvariavelcomnomegrande

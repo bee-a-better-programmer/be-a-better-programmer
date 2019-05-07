9cd825e6c25b3da23bd1dbb179996d320feff14c
Debora Duarte <debora.duarteas@gmail.com>
2019-05-07 14:04:41 -0300

    Delete exercise1.rb

--- confidence/exercise1.rb
+++
@@ -1,8 +0,0 @@
-# This code is written in Ruby. And this line is a comment in Ruby.
-# Create a ruby project on Repl.it.
-# Do not be afraid if you don't know ruby: just try fixing it. 
-# The suggestion is: run the code, take the error message and Google it to try to find out and fix the problem.
-
-umavariavelcomnomegrande = 1
-outravariavelcomnomegrande = 1 + umavariavelcomnomegrande
-puts outrvariavelcomnomegrande
